a3b18cc333f12538ff30d1e469aa05bbeacceea7
Bruno Alassia <vruno.alassia@gmail.com>
2013-11-20 21:55:37 -0300

    agrego evento click, muestra la latitude y longitud en los inputs

--- Gmaps.php
+++ Gmaps.php
@@ -9,6 +9,8 @@ class ZFE_Gmaps extends Zend_Form_Element {
 
 	private $_zoom = 14;
 
+	private $_marker = null;
+
 	public function setAppKey($appKey) {
 	
 		$this->_appKey = (string) $appKey;
@@ -27,6 +29,10 @@ class ZFE_Gmaps extends Zend_Form_Element {
 		return $this;
 	}
 
+	public function setMarker($img_path) {
+		$this->_marker = (string) $img_path;
+	}
+
 	public function render() {
 	
 		$xhtml = "";
@@ -36,13 +42,31 @@ class ZFE_Gmaps extends Zend_Form_Element {
 		$xhtml.= '<label>Longitud</label><input type="text" id="longitude-'.$this->getId().'" value="'.(float)$this->_longitude.'" /><br />';
 		$xhtml.= '<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key='.$this->_appKey.'&sensor=true"></script>';
 		$xhtml.= '<script type="text/javascript">';
+		$xhtml.= '	var map = null; var marker = null;';
+		$xhtml.= '  var myLatLng = new google.maps.LatLng('.(float)$this->_latitude.', '.$this->_longitude.');';
       	$xhtml.= '	function initialize() {';
 		$xhtml.= '		var mapOptions = {';
 		$xhtml.= '			center: new google.maps.LatLng('.(float)$this->_latitude.', '.(float)$this->_longitude.'),';
         $xhtml.= ' 			zoom: '.(int)$this->_zoom.',';
         $xhtml.= '		};';
-        $xhtml.= '		var map = new google.maps.Map(document.getElementById("'.$this->getId().'"), mapOptions);';
-      	$xhtml.= '	}';
+		$xhtml.= '		map = new google.maps.Map(document.getElementById("'.$this->getId().'"), mapOptions);';
+
+		if($this->_marker != null) {
+			$xhtml.= 'marker = new google.maps.Marker({';
+	    	$xhtml.= '	position: myLatLng,';
+		    $xhtml.= ' 	map: map,';
+    		$xhtml.= '	icon: "http://kuesty.com/resources/images/map_pin_me.png"';
+			$xhtml.= '});';
+		}
+
+		$xhtml.= 'google.maps.event.addListener(map, "click", function(event) {';
+		$xhtml.= '  document.getElementById("latitude-'.$this->getId().'").value = event.latLng.lat();';
+		$xhtml.= '  document.getElementById("longitude-'.$this->getId().'").value = event.latLng.lng();';
+  	    $xhtml.= '});';
+
+		$xhtml.= '	}';
+
+	
 		$xhtml.= '  google.maps.event.addDomListener(window, "load", initialize);';
 		$xhtml.= '</script>';
 
